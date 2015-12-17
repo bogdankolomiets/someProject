d3f757b4c3fa9d5965a6c090e092c2b59915a1d4
Bogdan Kolomiets <kolom996@gmail.com>
2015-12-17 16:26:50 +0200

    Refactoring Code

--- app/src/main/java/com/example/root/douclient/activity/FullArticleActivity.java
+++ app/src/main/java/com/example/root/douclient/activity/FullArticleActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,9 +22,10 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.*;
 
 /**
- * Created by root on 14.12.15.
+ * Created by bogdan on 14.12.15.
  */
 public class FullArticleActivity extends AppCompatActivity {
 
@@ -31,16 +33,15 @@ public class FullArticleActivity extends AppCompatActivity {
     private static final int LAYOUT_CONTENT_CONTAINER_ID = R.id.layoutContentContainer;
     private LinearLayout layoutContentContainer;
     private Toolbar toolbar;
-    private CollapsingToolbarLayout collapsingToolbarLayout;
-    private ImageView toolbarIcon;
     private String fullArticleURL;
     private String sArticleTitle;
+    private String sArticlePageTags = "";
+    private Elements eArticlePageTags;
     private static final String HTTP_DOU_UA = "http://dou.ua";
-    private String articleIconURL;
     private Elements articleContent;
-    private Elements eArticleContentImage;
-    private Elements articleHeading;
-    private Elements articleCode;
+    Pattern pattern = Pattern.compile("h\\d");
+    Matcher matcher;
+
     private Element eArticleTitle;
     private TextView textArticleContent;
     private ImageView imageContentArticle;
@@ -53,16 +54,10 @@ public class FullArticleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
         layoutContentContainer = (LinearLayout) findViewById(LAYOUT_CONTENT_CONTAINER_ID);
-
         fullArticleURL = HTTP_DOU_UA + getIntent().getExtras().getString("fullArticleURL");
-        articleIconURL = getIntent().getExtras().getString("articleIconURL");
-        System.out.println(fullArticleURL);
-        toolbarIcon = (ImageView) findViewById(R.id.fullArticleImage);
         toolbar = (Toolbar) findViewById(R.id.articleToolbar);
+        toolbar.setTitle("Статья");
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
 
         articleContentThread.execute();
 
@@ -78,27 +73,30 @@ public class FullArticleActivity extends AppCompatActivity {
                 HTMLPage = Jsoup.connect(fullArticleURL).get();
                 eArticleTitle = HTMLPage.select("article.b-typo h1").first();
                 sArticleTitle = eArticleTitle.text().replace("&nbsp;", " ");
-                articleContent = HTMLPage.select("article.b-typo p");
-                if (articleContent != null)
-                    for (Element element : articleContent) {
-                        contentElements.add(new NewsArticlePageElements("CONTENT", element.text().replace("&nbsp;", " ")));
+                eArticlePageTags = HTMLPage.select(".b-post-tags a");
+
+                for (Element element : eArticlePageTags) {
+                    sArticlePageTags += element.text() + ", ";
+                    System.out.println(sArticlePageTags);
+                }
+
+                articleContent = HTMLPage.select("article.b-typo div");
+                for (Element element : articleContent) {
+                    for(Element elementsArticleContent : element.children()) {
+                        matcher = pattern.matcher(elementsArticleContent.tagName());
+                        if (elementsArticleContent.tagName().equals("p") && elementsArticleContent.hasText()) {
+                            contentElements.add(new NewsArticlePageElements("CONTENT", elementsArticleContent.text().replace("&nbsp;", " ")));
+                        } else if (elementsArticleContent.children().is("img")) {
+                            String articleContentImageURL = elementsArticleContent.children().attr("src");
+                            contentElements.add(new NewsArticlePageElements("IMAGE", articleContentImageURL));
+                        } else if (matcher.matches()) {
+                            contentElements.add(new NewsArticlePageElements("CONTENT_HEADING", elementsArticleContent.text().replace("&nbsp;", " ")));
+                        } else if (elementsArticleContent.tagName().equals("pre")) {
+                            contentElements.add(new NewsArticlePageElements("CONTENT_CODE", elementsArticleContent.text()));
+                        }
                     }
-                eArticleContentImage = HTMLPage.select("article.b-typo p img");
-                if (eArticleContentImage != null)
-                    for (Element element : eArticleContentImage) {
-                        String articleContentImageURL = element.attr("src");
-                        contentElements.add(new NewsArticlePageElements("IMAGE", articleContentImageURL));
-                    }
-                articleHeading = HTMLPage.select("article.b-typo h2");
-                if (articleHeading != null)
-                    for (Element element : articleHeading) {
-                        contentElements.add(new NewsArticlePageElements("CONTENT_HEADING", element.text().replace("&nbsp;", " ")));
-                    }
-                articleCode = HTMLPage.select("article.b-typo pre");
-                if (articleCode != null)
-                    for (Element element : articleCode) {
-                        contentElements.add(new NewsArticlePageElements("CONTENT_CODE", element.text()));
-                    }
+
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -110,23 +108,28 @@ public class FullArticleActivity extends AppCompatActivity {
             super.onPostExecute(aVoid);
 
             LayoutInflater inflater = getLayoutInflater();
-            Picasso.with(getApplicationContext())
-                    .load(articleIconURL)
-                    .into(toolbarIcon);
-            collapsingToolbarLayout.setTitle(sArticleTitle);
+
+            textArticleContent = (TextView) inflater.inflate(R.layout.title_article_page, null);
+            textArticleContent.setText(sArticleTitle);
+            layoutContentContainer.addView(textArticleContent);
+
+            textArticleContent = (TextView) inflater.inflate(R.layout.text_article_content, null);
+            textArticleContent.setTextColor(R.color.darkGrey);
+            textArticleContent.setText(sArticlePageTags);
+            layoutContentContainer.addView(textArticleContent);
 
             for (NewsArticlePageElements element : contentElements) {
-                if (element.getElementType() == "CONTENT") {
+                if (element.getElementType().equals("CONTENT")) {
                     textArticleContent = (TextView) inflater.inflate(R.layout.text_article_content, null);
                     textArticleContent.setText(element.getElementContent());
                     layoutContentContainer.addView(textArticleContent);
-                } else if (element.getElementType() == "IMAGE") {
+                } else if (element.getElementType().equals("IMAGE")) {
                     imageContentArticle = (ImageView) inflater.inflate(R.layout.image_article_content, null);
                     Picasso.with(getApplicationContext())
                             .load(element.getElementContent())
                             .into(imageContentArticle);
                     layoutContentContainer.addView(imageContentArticle);
-                } else if (element.getElementType() == "CONTENT_HEADING") {
+                } else if (element.getElementType().equals("CONTENT_HEADING")) {
                     textArticleContent = (TextView) inflater.inflate(R.layout.text_heading_article_content, null);
                     textArticleContent.setText(element.getElementContent());
                     layoutContentContainer.addView(textArticleContent);
@@ -134,12 +137,6 @@ public class FullArticleActivity extends AppCompatActivity {
 
             }
 
-//            for (Element element : articleContent) {
-//
-//                textArticleContent = (TextView) inflater.inflate(R.layout.text_article_content, null);
-//                textArticleContent.setText(element.text().replace("&nbsp;", " "));
-//                layoutContentContainer.addView(textArticleContent);
-//            }
         }
 
     }
